6c7ca00f2511f236671ac02c89f59ff2a7c34a1e
Firmino Changani <firmino.changani@gmail.com>
2021-08-27 18:17:24 +0100

    chore: remove demo from app and add global styles

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,13 @@
   <aside>
     <div></div>
     <nav></nav>
-    <app-demo></app-demo>
   </aside>
   <router-view />
 </template>
 
 <script>
-import Demo from "@/common/components/Demo.vue";
 import { defineComponent } from "vue";
-export default defineComponent({
-  components: { "app-demo": Demo },
-});
+export default defineComponent({});
 </script>
 
 <style lang="scss"></style>
