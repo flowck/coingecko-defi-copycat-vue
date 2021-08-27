d7361fa3b129b99222f84c6dfaa8e49d7a1cd08b
Firmino Changani <firmino.changani@gmail.com>
2021-08-27 21:18:59 +0100

    chore(#4): implement stat componen
    - Add a new component file
    - Add unit test file

--- src/modules/defiCoins/views/ListDeFiCoins.vue
+++ src/modules/defiCoins/views/ListDeFiCoins.vue
@@ -1,9 +1,16 @@
 <template>
-  <section>List coins per market</section>
+  <section>
+    List coins per market
+    <base-stat value="$379,205,276,056" label="ETH Market Cap"></base-stat>
+  </section>
 </template>
 
 <script lang="ts">
+import BaseStat from "@/common/components/base-stat.vue";
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+  name: "ListDeFiCoins",
+  components: { BaseStat },
+});
 </script>
