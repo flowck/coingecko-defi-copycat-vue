7199423102179b70971103cfb61e8e9b3923d96d
Firmino Changani <firmino.changani@gmail.com>
2021-08-29 18:15:41 +0100

    chore(#1): add axios
    - install and setup axios
    - setup prettier
    - mock axios

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,8 @@ import { store } from "./store";
 import { createApp } from "vue";
 import { router } from "./router";
 
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.VUE_APP_COINGECKO_API;
+
 createApp(App).use(store).use(router).mount("#app");
