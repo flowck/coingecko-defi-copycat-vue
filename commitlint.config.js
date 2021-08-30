c3045cfaaaa9ac2fe62d27841c8af6b1374200c4
Firmino Changani <firmino.changani@gmail.com>
2021-08-30 21:09:33 +0100

    fix: disable commitlint rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "body-max-length": [0, "never", Infinity],
+    "body-max-line-length": [0, "never", Infinity],
+  },
+};
