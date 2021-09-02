832738246053145c8b2bdaf166b7929148e9f4c9
Firmino Changani <firmino.changani@gmail.com>
2021-09-02 17:57:24 +0100

    feat: format currency in the time-series chart

--- src/common/components/time-series-chart/time-series-chart.vue
+++ src/common/components/time-series-chart/time-series-chart.vue
@@ -7,7 +7,9 @@
 <script lang="ts">
 import { Chart } from "highcharts-vue";
 import { defineComponent, PropType } from "vue";
+import { numberToCurrency } from "@/common/utils/currency";
 import { HistoricalData } from "@/modules/coins/store/coins.interfaces";
+import { AxisLabelsFormatterContextObject, Time } from "highcharts";
 
 export default defineComponent({
   components: {
@@ -18,9 +20,22 @@ export default defineComponent({
       type: Object as PropType<HistoricalData>,
       required: true,
     },
+    currency: { type: String, required: true },
   },
   computed: {
+    currencySymbol(): string {
+      const currency = this.currency.toUpperCase();
+      const symbols: Record<string, string> = {
+        USD: "$",
+        EUR: "€",
+        GBP: "£",
+      };
+
+      return symbols[currency] || currency;
+    },
     chartOptions() {
+      const currency = this.currency;
+      const timestampToDate = this.timestampToDate;
       return {
         chart: { zoomType: "x" },
         credits: {
@@ -37,12 +52,29 @@ export default defineComponent({
         },
         yAxis: {
           labels: {
-            format: "${value:.2f}",
+            formatter: (_self: AxisLabelsFormatterContextObject) => {
+              return numberToCurrency(_self.value as number, this.currency);
+            },
           },
           title: "",
         },
         series: [this.series],
+        tooltip: {
+          formatter: function () {
+            const _self = this as unknown as Record<string, number>;
+            return ` <b>${numberToCurrency(
+              _self.y,
+              currency
+            )}</b> <br /> ${timestampToDate(_self.x)}`;
+          },
+        },
       };
+    },
+  },
+  methods: {
+    timestampToDate(value: number): string {
+      const time = new Time({});
+      return time.dateFormat("%Y-%m-%d %H:%M:%S", value);
     },
   },
 });
